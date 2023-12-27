4f64ff7c22d95d96027faf82d09eebcb115b99a3
private readonly <oncwnuGUl3ArIcoMlZ8DPyir72Js@git.weixin.qq.com>
2023-12-27 22:17:04 +0800

    Initial commit

---
+++ src/renderer/index.tsx
@@ -0,0 +1,5 @@
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+root.render(<h2>Hello from React!</h2>);
